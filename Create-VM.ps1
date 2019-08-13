03bb866ff097adbc4a301ec9849bad9bf7710932
sunny <sunnyjamwal@gmail.com>
2019-08-12 23:31:24 -0400

    adding param

--- Create-VM.ps1
+++ Create-VM.ps1
@@ -1,3 +1,28 @@
-Connect-AzAccount
 
-New-AzResourceGroup -Name "Sunny-Test" -Location "canadaeast"
+function Create-VM {
+    param (
+        [Parameter(Mandatory=$true,
+        Position=0)]
+        [string]
+        $RSGName,
+       
+
+        [Parameter(Mandatory=$true,
+        Position=1)]
+        [String]
+        $Location
+    )
+
+    try {
+        Connect-AzAccount
+    }
+    catch {
+        Write-Host "Error Connecting"
+    }
+    
+
+    New-AzResourceGroup -Name $RSGName -Location $Location
+
+    
+}
+#Create-VM "test-sunny" "canadaeast"
